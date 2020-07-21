9d38f64d5f93d6c58ec06d42d4eff7b6d655abc1
Rehan <idrisi.rehan@gmail.com>
2020-07-21 16:04:42 +0530

    Final Upload For Assigned Task

---
+++ response.js
@@ -0,0 +1,68 @@
+var savedUsers = {};
+$(document).ready(function () {
+  fetchUserData();
+  function fetchUserData() {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+      .then((response) => response.json())
+      .then((users) => {
+        let output = "";
+        users.forEach(function (user) {
+          output += `
+        <tr class='py-0 searchList'>
+        <th scope='row' >${user.id}</th>
+        <td class='text-center'> 
+        <div class="custom-control custom-checkbox custom-control-inline">
+        <input type="checkbox" class="custom-control-input userSelector" id="User${user.id}" data-userid=${user.id}>
+        <label class="custom-control-label" for="User${user.id}"></label>
+        </div>
+        </td>
+        <td>${user.name}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+        <td><button type='button' class='btn indigo white-text btn-sm fetchUser' data-toggle='modal' data-target='#userModal' data-userid='${user.id}'>
+        <i class="fas fa-info-circle fa-2x"></i>
+        </button></td></tr>
+        `;
+        });
+
+        $("#userList").append(output);
+      });
+  }
+  $(document.body).on("click", ".fetchUser", function () {
+    $userId = $(this).data("userid");
+    fetch("https://jsonplaceholder.typicode.com/users/" + $userId)
+      .then((response) => response.json())
+      .then((users) => {
+        let output = `
+        <p>Name : ${users.name}</p>
+        <p>Username : ${users.username}</p>
+        <p>Email : ${users.email}</p>
+        <p>Address : ${users.address.street}, ${users.address.suite}, ${users.address.city},
+            ${users.address.zipcode}</p>
+        <p>Phone : ${users.phone}</p>
+        <p>Website : ${users.website}</p>
+        <p>Company : ${users.company.name} <em>${users.company.catchPhrase}</em> </p>
+        <p class='ml-5'> ${users.company.bs}</p>
+        `;
+        $(".modal-body").html(output);
+        $(".modal-title").html(`${users.name}`);
+      });
+  });
+  $(document.body).on("change", ".userSelector", function () {
+    savedUsers[$(this).data("userid") + "userId"] = $(this).data("userid");
+    // savedUsers["user"] = $(this).data("userid");
+  });
+  $(document.body).on("click", "#saveUsers", function () {
+    console.log(savedUsers);
+    $(".userSelector").each(function (index) {
+      $(this).prop("checked", false);
+    });
+    $("#popUpMessage").modal("show");
+  });
+  $(".filterSearch").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".searchList").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
